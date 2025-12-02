bcf48bc3dd46f63025068723383d5061927edbc2
valeriascenario <valeria@scenario.ind.br>
2025-12-02 16:04:38 -0300

    creating title

--- app/src/main/java/com/example/supercompras/MainActivity.kt
+++ app/src/main/java/com/example/supercompras/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,14 +19,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperComprasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Valéria",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Title()
                 }
             }
         }
     }
+}
+
+@Composable
+fun Title(modifier: Modifier = Modifier) {
+    Text(text = "Lista de Compras")
 }
 
 @Composable
@@ -36,6 +37,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Preview
+@Composable
+private fun TitlePreview() {
+    SuperComprasTheme{
+        Title()
+    }
 }
 
 @Preview(showBackground = true)
